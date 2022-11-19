7b77ff62138facc684218be779ca60a53f6f5665
Anette-Gallegos <68409047+Anette-Gallegos@users.noreply.github.com>
2022-11-19 13:23:19 -0500

    Act

--- Ejercicios/investigadores.sql
+++ Ejercicios/investigadores.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-11-2022 a las 12:31:04
+-- Tiempo de generación: 19-11-2022 a las 19:21:53
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `investigadores`
 --
+
+create database investigadores;
 
 -- --------------------------------------------------------
 
@@ -64,7 +66,9 @@ INSERT INTO `facultad` (`Codigo`, `Nombre`) VALUES
 (2, 'Facultad002'),
 (3, 'Facultad003'),
 (4, 'Facultad004'),
-(5, 'Facultad005');
+(5, 'Facultad005'),
+(6, 'Facultad006'),
+(7, 'Facultad007');
 
 -- --------------------------------------------------------
 
@@ -83,11 +87,13 @@ CREATE TABLE `investigadores` (
 --
 
 INSERT INTO `investigadores` (`DNI`, `NomApels`, `Facultad`) VALUES
-('00000001', 'Juan Perez', 5),
-('00000002', 'Maria Lupa', 4),
+('00000001', 'Juan Perez', 1),
+('00000002', 'Maria Lupa', 2),
 ('00000003', 'Pancrasio Juarez', 3),
-('00000004', 'Jose Jose', 2),
-('00000005', 'Chayanne', 1);
+('00000004', 'Jose Jose', 4),
+('00000005', 'Chayanne', 5),
+('00000006', 'Hanna Banana', 7),
+('00000007', 'Zacarías Flores Del Campo', 6);
 
 -- --------------------------------------------------------
 
@@ -111,7 +117,9 @@ INSERT INTO `reserva` (`DNI`, `NumSerie`, `Comienzo`, `Fin`) VALUES
 ('00000002', 'efgh', '2021-11-26 10:41:39', '2022-11-30 09:35:03'),
 ('00000001', 'ijkl', '2022-12-13 11:10:03', '2022-12-31 14:10:03'),
 ('00000001', 'mnop', '2022-12-31 14:44:26', '2023-01-25 11:10:03'),
-('00000001', 'qrst', '2022-12-05 12:10:03', '2022-12-10 19:10:03');
+('00000001', 'qrst', '2022-12-05 12:10:03', '2022-12-10 19:10:03'),
+('00000004', 'mnop', '2022-11-01 13:18:11', '2022-11-30 13:18:11'),
+('00000004', 'efgh', '2022-11-30 13:18:11', '2022-12-31 13:18:11');
 
 --
 -- Índices para tablas volcadas
